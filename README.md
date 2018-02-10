f4c3e36411a67e2ca7516f6c9b041d6f4dcc4897
Andreas Radke <radkeandreas@gmail.com>
2018-02-10 18:39:49 +0100

    edit README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Ant-Colony-Optimization
+Authors: Moran Gybels and Andreas Radke
+
+
+Requirements: 	Python 3.6+
+		Numpy
+		Pandas
+
+USAGE:
+python cities_of_europe.py
+
+If you want to change the parameters open cities_of_europe.py and change the parameters for nr_ants, nr_best etc. The parameters are explained in the antcolony.py and the AntColony class.
--- cities_of_europe.py
+++ cities_of_europe.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import pandas as pd
 
@@ -9,8 +10,12 @@ distances = distances.replace('-', np.inf)  # diagonal should be infinity
 distances = distances.apply(pd.to_numeric, errors='coerce')  # convert strings in dataframe to floats
 distances = distances.values[:, 1:]  # take only the columns and rows with values, i.e. remove city name column
 
-ant_colony = AntColony(distances, nr_ants=40, nr_best=10, nr_iterations=200, decay=0.95, alpha=1, beta=1, phero_min=0,
+
+ant_colony = AntColony(distances, nr_ants=20, nr_best=10, nr_iterations=500, decay=0.95,
+                       alpha=1, beta=1, phero_min=0.1,
                        phero_max=1, nr_procs=6)
 shortest_path = ant_colony.run()
 print(f"shortest_path: {shortest_path}")
-print(f"Distance: {shortest_path[1]}")
+
+
+
